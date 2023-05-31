428fab3617912267312556db2926e75dd382d6db
Djok64 <joakym.anc@gmail.com>
2023-05-31 22:34:53 +0200

    Challenge React basics 04  passage d'un pokémon de la liste avec un prop pokemon

--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -1,5 +1,6 @@
-function PokemonCard() {
-    const pokemon = pokemonList[1]; //prend le premier pokemon de pokemonList//
+function PokemonCard(props) {
+    console.log(props)
+    const pokemon = props.pokemon//prend le premier pokemon de pokemonList//
      return (// ce que j'avai penser ( pokemon.imgSrc === null )? <p>???</p>:(        la realité en dessous ....
          <figure>
             {pokemon.imgSrc != undefined ?(// on pourrais également écrire  a cette ligne {pokemon.imgSrc ?( qui revient a demander simplement si elle existe ou si elle est undefined  on verifie ce qui ne fontionne pas en premier
@@ -16,15 +17,6 @@ function PokemonCard() {
     
 }
 
-const pokemonList = [
-    {
-        name:"bulbasaur",
-        imgSrc:
-        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
-    },
-    {
-        name: "mew",
-    },
-];
+
 
 export default PokemonCard;
