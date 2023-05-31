7907ce0c85cb2153c9b9150aeddc9e712e7e3e58
Djok64 <joakym.anc@gmail.com>
2023-05-31 11:15:45 +0200

    modification de la pokemon card en y ajoutant une pokemonList + faire afficher l'objet du tableau pokemonList si il ne contient pas d'image src il affichera quand même le nom mais de ???? apparaitrons a la place de l'image

--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -1,13 +1,30 @@
 function PokemonCard() {
-    return(
+    const pokemon = pokemonList[1]; //prend le premier pokemon de pokemonList//
+     return (// ce que j'avai penser ( pokemon.imgSrc === null )? <p>???</p>:(        la realité en dessous ....
          <figure>
+            {pokemon.imgSrc ?(//je ne comprend pas bien cette ligne  if pokemon.imgSrc ??? if quoi? 
         <img
-         src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png" 
-         alt="bulbasaur"
-         />
-            <figcaption>bulbasaur</figcaption>
+         src={pokemon.imgSrc}
+         alt={pokemon.name}
+         />  //si(if le point d'interogation l.5) il ya une pokemon.imgSrc (il affiche l'image  )
+            ) : (  // les : correspondent a sinon  
+                <p>???</p> //il affichera ceci 
+            )}
+            <figcaption>{pokemon.name}</figcaption>
     </figure>
     );
+    
 }
+
+const pokemonList = [
+    {
+        name:"bulbasaur",
+        imgSrc:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+    },
+    {
+        name: "mew",
+    },
+];
 
 export default PokemonCard;
