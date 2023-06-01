fcbc30710d8391c71bc7e0a39f6d275a4bcd60db
Djok64 <joakym.anc@gmail.com>
2023-06-01 19:05:49 +0200

    ajout d'un bouton précédent et suivant a mon pokedex , le bouton disparait si il n'ya pas de valeur avant ou ensuite

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,51 @@
 import PokemonCard from './components/PokemonCard'
 import "./App.css"
+import { useState } from 'react';
 
 const pokemonList = [
   {
-      name:"bulbasaur",
+      name: "bulbasaur",
       imgSrc:
-      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
-  },
-  {
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+    },
+    {
+      name: "charmander",
+      imgSrc:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/4.png",
+    },
+    {
+      name: "squirtle",
+      imgSrc:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/7.png",
+    },
+    {
+      name: "pikachu",
+      imgSrc:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/25.png",
+    },
+    {
+      
       name: "mew",
-  },
-];
+    },
+  ];
 
 
 function App() {
   
+  const [pokemonIndex, setPokemonIndex] = useState(0);
+  const previousClick = () => setPokemonIndex(pokemonIndex - 1 );
+  const nextClick = () => setPokemonIndex(pokemonIndex + 1);
 
   return (
     <>
-    <PokemonCard pokemon={pokemonList[0]}/>
+      <PokemonCard pokemon={pokemonList[pokemonIndex]}/>
+      {pokemonIndex > 0 ? (
+      <input type="button" value="Précédent" onClick={previousClick}/>
+       ): null }
+       {pokemonIndex < pokemonList.length -1 ? (
+      <input type="button" value="Suivant" onClick={nextClick}/> 
+       ) : null } 
+                  
     </>
   )
 }
